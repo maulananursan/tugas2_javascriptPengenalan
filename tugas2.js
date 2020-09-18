2256d8bd7ba65d03299e9b4a783b3a5553ec4f92
Maulana Nursan <71365575+maulananursan@users.noreply.github.com>
2020-09-18 10:44:29 +0700

    Add files via upload

---
+++ tugas2.js
@@ -0,0 +1,13 @@
+var angka = 1
+var teks = "dua"
+var boolean = true
+var nul = null;
+var undev;
+var array = {siji : 1, loro : "dua", telu : "tiga" }
+
+console.log(typeof angka)
+console.log(typeof teks)
+console.log(typeof boolean)
+console.log(typeof nul)
+console.log(typeof undev)
+console.log(typeof array)
